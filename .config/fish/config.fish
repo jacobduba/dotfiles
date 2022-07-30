23dd8c53e0d9b39c62e7199d3b6d8d6a29d79fd3
Jacob Duba <jacob@mooshoe.me>
2022-07-30 17:58:59 -0500

    Informative fish prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,9 @@
+# Prompt
 set fish_greeting
+fish_vi_key_bindings
 
-set -gx PATH $HOME/bin $PATH $HOME/go/bin
+set -gx EDITOR code
 
-set -gx EDITOR nvim
-set -gx BROWSER firefox
-set -gx TERMINAL termite
-
-set -gx BEMENU_OPTS "-l 10 --ignorecase -n --fn 'Fira Code 11' --tf '#FFFFFF' --hf '#FFFFFF'"
-set -gx MOZ_ENABLE_WAYLAND 1
-set -gx _JAVA_AWT_WM_NONREPARENTING 1
-set -gx QT_QPA_PLATFORMTHEME qt5ct
-
-alias r="ranger"
 alias ll="ls -lh"
 
 alias fishconf "$EDITOR ~/.config/fish/config.fish"
@@ -20,9 +12,8 @@ alias swayconf "$EDITOR ~/.config/sway/config"
 alias todo "$EDITOR ~/OneDrive/todo.md"
 alias workouts "$EDITOR ~/OneDrive/workouts.md"
 
+# Go
+set -gx PATH $HOME/bin $PATH $HOME/go/bin
+
 # Nix
 fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-
-if status is-interactive
-
-end
