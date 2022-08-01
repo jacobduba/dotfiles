1f3ec40b9669110628b9873bcd53704e59fd7a6f
Jacob Duba <jacob@mooshoe.me>
2022-08-01 09:18:25 -0500

    Add Souces AWS bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,7 +17,16 @@ alias workouts "$EDITOR ~/OneDrive/workouts.md"
 set -gx PATH $HOME/bin $PATH $HOME/go/bin
 
 # Nix
-fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+# fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 
-# Homebrew
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# Linuxbrew
+# eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+# Source Allies
+set cli_dir "~/Documents/sai-aws-auth/cli-auth"
+alias dev "pushd $cli_dir && fenv source login-aws-dev.sh && popd"
+alias qual "pushd $cli_dir && fenv source login-aws-qual.sh && popd"
+alias prod "pushd $cli_dir && fenv source login-aws-prod.sh && popd"
+alias shared "pushd $cli_dir && fenv source login-aws-shared.sh && popd"
+alias sandbox.dev "pushd $cli_dir && fenv source login-aws-sandbox-dev.sh && popd"
+alias sandbox.prod "pushd $cli_dir && fenv source $cli_dir/login-aws-sandbox-prod.sh && popd"
