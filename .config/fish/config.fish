3dfd68458bb2d490f95befd2f77a1a8eca326dae
Jacob Duba <jacob@mooshoe.me>
2022-07-31 21:16:24 -0500

    add vscode config since im using that now

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,18 +2,22 @@
 set fish_greeting
 fish_vi_key_bindings
 
+# Defaults
 set -gx EDITOR code
 
+# Aliases
 alias ll="ls -lh"
-
 alias fishconf "$EDITOR ~/.config/fish/config.fish"
 alias vimrc "$EDITOR ~/.config/nvim/init.vim"
 alias swayconf "$EDITOR ~/.config/sway/config"
 alias todo "$EDITOR ~/OneDrive/todo.md"
 alias workouts "$EDITOR ~/OneDrive/workouts.md"
 
-# Go
+# Gopath
 set -gx PATH $HOME/bin $PATH $HOME/go/bin
 
 # Nix
 fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+
+# Homebrew
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
