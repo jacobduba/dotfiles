96480070227ac65194f26a048ddb580e43cf3855
Jacob Duba <jacob@mooshoe.me>
2022-08-06 21:05:30 -0500

    updated to reflect current workflow
    added test before creating aliases for sai-aws-auth
    added fisher
    
    changed vscode to use control center
    changed vscode keybindings to switch terminals using vim bindings
    
    removed .bin scripts
    removed sway (on gnome now)
    removed i3status
    removed alacritty (using gnome-console)
    removed foot
    removed aws completions from version tracking (handled by fisher)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,20 @@
-# Prompt
+# Shell
 set fish_greeting
 fish_vi_key_bindings
 
-# Defaults
+# Enviroment
 set -gx EDITOR code
+fish_add_path $HOME/go/bin $HOME/.local/bin
 
 # Aliases
 alias ll="ls -lh"
 alias fishconf "$EDITOR ~/.config/fish/config.fish"
-alias vimrc "$EDITOR ~/.config/nvim/init.vim"
-alias swayconf "$EDITOR ~/.config/sway/config"
-alias todo "$EDITOR ~/OneDrive/todo.md"
-alias workouts "$EDITOR ~/OneDrive/workouts.md"
-
-# Gopath
-set -gx PATH $HOME/bin $PATH $HOME/go/bin
-
-# Nix
-# fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-
-# Linuxbrew
-# eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 # Source Allies
-set cli_dir "~/Documents/sai-aws-auth/cli-auth"
-alias dev "pushd $cli_dir && fenv source login-aws-dev.sh && popd"
-alias qual "pushd $cli_dir && fenv source login-aws-qual.sh && popd"
-alias prod "pushd $cli_dir && fenv source login-aws-prod.sh && popd"
-alias shared "pushd $cli_dir && fenv source login-aws-shared.sh && popd"
-alias sandbox.dev "pushd $cli_dir && fenv source login-aws-sandbox-dev.sh && popd"
-alias sandbox.prod "pushd $cli_dir && fenv source $cli_dir/login-aws-sandbox-prod.sh && popd"
+if test -d ~/Documents/sai-aws-auth
+    set cli_dir "~/Documents/sai-aws-auth/cli-auth"
+    alias dev "pushd $cli_dir && fenv source login-aws-dev.sh && popd"
+    alias qual "pushd $cli_dir && fenv source login-aws-qual.sh && popd"
+    alias prod "pushd $cli_dir && fenv source login-aws-prod.sh && popd"
+    alias shared "pushd $cli_dir && fenv source login-aws-shared.sh && popd"
+end
