6e381ac457ed0d17af87eb88a1855729f8ca987b
Jacob Duba <jacob@mooshoe.me>
2023-03-16 03:07:59 -0500

    Removed flutter source

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ fish_vi_key_bindings
 
 # Enviroment
 set -gx EDITOR nvim
-fish_add_path $HOME/.local/bin $HOME/bench/flutter/bin $HOME/Android/cmdline-tools/latest/bin
+fish_add_path $HOME/.local/bin
 
 # Aliases
 alias fishconf "$EDITOR ~/.config/fish/config.fish"
