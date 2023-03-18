70761916e14c7f56ee2a6dd68a659c79f31c53a4
Jacob Duba <jacob@mooshoe.me>
2023-03-18 17:22:15 -0500

    Change cursor in shell based on vi mode

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,12 @@
-# Shell
+# Remove greeting
 set fish_greeting
+
+# Vi bindings
 fish_vi_key_bindings
+set fish_cursor_default     block      #blink
+set fish_cursor_insert      line       #blink
+set fish_cursor_replace_one underscore #blink
+set fish_cursor_visual      block
 
 # Enviroment
 set -gx EDITOR nvim
@@ -8,6 +14,7 @@ fish_add_path $HOME/.local/bin
 
 # Aliases
 alias cat "bat"
+alias docker "podman -r"
 alias podman "podman -r"
 alias fishconf "$EDITOR ~/.config/fish/config.fish"
 alias vimrc "$EDITOR ~/.config/nvim/init.vim"
